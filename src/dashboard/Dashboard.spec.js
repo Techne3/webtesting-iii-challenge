d1e993dce5f57b50cff9913da63cb26cab986ec2
Timothy Sharkey <Techne3@gmail.com>
2019-10-14 14:51:44 -0600

    Tested snapshot render on the three components tested fireEvent click on Control and Display components

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,9 @@
 // Test away
+import React from 'react';
+import { render } from '@testing-library/react';
+import Dashboard from './Dashboard'
+
+
+test('Control is rendering correctly.', () => {
+    expect(render(<Dashboard />)).toMatchSnapshot();
+});
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,47 @@
 // Test away!
+
+import React from 'react';
+import { render } from '@testing-library/react';
+import Display from './Display'
+
+
+test('Control is rendering correctly.', () => {
+    expect(render(<Display />)).toMatchSnapshot();
+});
+
+
+test ('test if its unlocked and open', ()=>{
+    const { getByText } = render(<Display />)
+  
+    const open = getByText(/open/i);
+    const unlocked = getByText(/unlocked/i);
+  
+    expect(open.className).toMatch(/green-led/i);
+    expect(unlocked.className).toMatch(/green-led/i);
+})
+
+
+test('test to see when locked and closed', () => {
+    const {getByText} = render(
+      <Display closed={true} locked={true} />
+    )
+
+    const closedDoor = getByText(/closed/i)
+    const lockedDoor = getByText(/locked/i)
+
+    expect(closedDoor.className).toMatch(/red-led/i)
+    expect(lockedDoor.className).toMatch(/red-led/i)
+})
+
+test('test to see unlocked and closed', ()=>{
+    const { getByText } = render(
+    <Display closed={true} locked={false}/>
+    )
+  
+    const closeDoor = getByText(/closed/i);
+    const unlockDoor = getByText(/unlocked/i);
+  
+    expect(closeDoor.className).toMatch(/red-led/i);
+    expect(unlockDoor.className).toMatch(/green-led/i);
+  })
+  
